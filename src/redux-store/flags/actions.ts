3fbb284d78040721694ed5fbcee926339e2c8c77
Phil Mashiane <mashianephil@gmail.com>
2019-10-13 21:14:47 +0200

    Created reset flags actions

--- src/redux-store/flags/actions.ts
+++ src/redux-store/flags/actions.ts
@@ -1,7 +1,18 @@
 import { createAction } from 'redux-actions';
-import { SET_IS_IN_PROGRESS_FLAG, SET_SUCCEEDED_FLAG, SET_FAILED_FLAG, SET_ACTIONED_FLAG } from './constants';
+import {
+  SET_IS_IN_PROGRESS_FLAG,
+  SET_SUCCEEDED_FLAG,
+  SET_FAILED_FLAG,
+  SET_ACTIONED_FLAG,
+  RESET_IS_IN_PROGRESS_FLAGS,
+  RESET_SUCCEEDED_FLAGS,
+  RESET_FAILED_FLAGS,
+  RESET_ACTIONED_FLAGS,
+  RESET_ALL_FLAGS,
+} from './constants';
 import { IFlagsState, IFlagProgressFlags, IFlagSucceededFlags, IFlagFailedFlags, IFlagActionedFlags } from './state';
 
+//#region Set flags
 export const setIsInProgressFlag = createAction<IFlagsState, { [key in IFlagProgressFlags]?: boolean }>(
   SET_IS_IN_PROGRESS_FLAG,
   key => ({
@@ -29,3 +40,16 @@ export const setActionedFlag = createAction<IFlagsState, { [key in IFlagActioned
     actioned: key,
   })
 );
+//#endregion
+
+//#region Reset flags
+export const resetIsInProgressFlag = createAction<IFlagsState>(RESET_IS_IN_PROGRESS_FLAGS);
+
+export const resetSucceededFlag = createAction<IFlagsState>(RESET_SUCCEEDED_FLAGS);
+
+export const resetFailedFlag = createAction<IFlagsState>(RESET_FAILED_FLAGS);
+
+export const resetActionedFlag = createAction<IFlagsState>(RESET_ACTIONED_FLAGS);
+
+export const resetAllFlag = createAction<IFlagsState>(RESET_ALL_FLAGS);
+//#endregion
