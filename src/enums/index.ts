bd46b10d2d10b7a8554f831217d4fba4b10d2761
Phil Mashiane <mashianephil@gmail.com>
2020-04-13 19:31:54 +0200

    Added some enums

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,1 +1,4 @@
 export { GaEventCategory } from './gaEventCategory';
+export { RouteEvents } from './routeEvents';
+export { HttpStatusCodes } from './httpStatusCodes';
+/* NEW_ENUM_EXPORT_GOES_HERE */
---
+++ src/enums/routeEvents.ts
@@ -0,0 +1,8 @@
+export enum RouteEvents {
+  RouteChangeStart = 'routeChangeStart',
+  RouteChangeComplete = 'routeChangeComplete',
+  RouteChangeError = 'routeChangeError',
+  BeforeHistoryChange = 'beforeHistoryChange',
+  HashChangeStart = 'hashChangeStart',
+  HashChangeComplete = 'hashChangeComplete',
+}
