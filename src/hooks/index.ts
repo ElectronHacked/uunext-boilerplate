b456da788110c6e6078ce5665b4dd033f73ad3da
Phil Mashiane <mashianephil@gmail.com>
2020-04-13 19:32:11 +0200

    Added some hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { usePrevious } from './usePrevious';
+export { useOnlineStatus } from './useOnlineStatus';
+export { useToggle } from './useToggle';
+export { useBoolean } from './useBoolean';
+/* NEW_HOOK_EXPORT_GOES_HERE */
